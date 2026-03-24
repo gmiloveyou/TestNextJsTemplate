892cda21412c3da39f565febea9a0ac4d6b77ae7
WNEWEB\25003860 <steven.hw.lin@wnc.com.tw>
2026-03-24 08:43:12 +0800

    chore: add experimental SRI configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    sri: {
+      algorithm: 'sha384', // or 'sha384' or 'sha512'
+    },
+  },
   output: 'export',
 
 };
